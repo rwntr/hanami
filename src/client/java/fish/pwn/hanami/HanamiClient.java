22497a1aa492a02a9dbe00f8aae011df0784f276
bunny winter <pyuku@pm.me>
2025-03-04 18:20:14 -0600

    initial commit

---
+++ src/client/java/fish/pwn/hanami/HanamiClient.java
@@ -0,0 +1,10 @@
+package fish.pwn.hanami;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class HanamiClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/fish/pwn/hanami/HanamiDataGenerator.java
@@ -0,0 +1,11 @@
+package fish.pwn.hanami;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HanamiDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
